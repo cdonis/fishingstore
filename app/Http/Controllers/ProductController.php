2bc9012a4e1ffacc072cdb4f6291ed502e00e2d9
Carlos Donis <cdonisdiaz@gmail.com>
2023-01-05 01:34:49 -0500

    Feature: massive stock update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateStockRequest;
 use App\Models\Product;
 use App\Services\ProductServices;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -78,5 +81,32 @@ class ProductController extends Controller
     {
         $product->delete();
         return response()->noContent();
+    }
+
+    /**
+     * Massive stock update
+     * 
+     * @param Illuminate\Http\UpdateStockRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function updateStock(UpdateStockRequest $request) 
+    {
+        // Construct and use a single raw SQL to avoid using one query for each product in the list
+        
+        $productTable = Product::getModel()->getTable();      // The product table
+        $cases = [];                                          // Conditions to use in the CASE clause
+        $ids = [];                                            // Set of IDs for the WHERE clause
+
+        $data = $request->validated();
+        foreach ($data['products'] as $product) {
+            $id = (int) $product['id'];
+            $cases[] = "WHEN {$id} then stock + {$product['quantity']}";
+            $ids[] = $id;
+        }
+
+        $ids = implode(',', $ids);
+        $cases = implode(' ', $cases);
+
+        return DB::update("UPDATE {$productTable} SET stock = CASE id {$cases} END WHERE id in ({$ids})");
     }
 }
